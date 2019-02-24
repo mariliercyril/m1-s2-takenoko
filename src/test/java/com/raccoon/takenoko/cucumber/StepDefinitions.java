5c568c0aeaa85e73832417f42c30e98f4f35c6e0
mariliercyril <cyril.marilier@etu.unice.fr>
2019-02-24 12:36:07 +0100

    Generalise the Cucumber test (#4)

--- src/test/java/com/raccoon/takenoko/cucumber/StepDefinitions.java
+++ src/test/java/com/raccoon/takenoko/cucumber/StepDefinitions.java
@@ -1,7 +1,5 @@
 package com.raccoon.takenoko.cucumber;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -9,10 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.raccoon.takenoko.game.objective.PandaObjective;
+
 import com.raccoon.takenoko.game.tiles.Color;
+
 import com.raccoon.takenoko.player.Player;
 
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -20,45 +19,49 @@ import cucumber.api.java.en.When;
 public class StepDefinitions {
 
 	private static Player mockPlayer;
-	private Map<Color, Integer> stomach;
-	
-	private PandaObjective pandaObjective = new PandaObjective(PandaObjective.Motif.ORIGINAL_GREEN);
-	
-	@Before
-	public void initialize() {
 
-		// Creates a stomach and initializes it
-		stomach = new HashMap<>();
-        stomach.put(Color.GREEN, 0);
-        stomach.put(Color.YELLOW, 0);
-        stomach.put(Color.PINK, 0);
-	}
-	
+	private Map<Color, Integer> stomach = new HashMap<>();
+
+	private PandaObjective pandaObjective;
+
 	@Given("^I am a player$")
 	public void i_am_a_player() {
-		
+
 		mockPlayer = mock(Player.class);
-		
+
 	}
 
-	@Given("^I have eaten (\\d+) green bamboos$")
-	public void i_have_eaten_green_bamboos(Integer nbBamboos) {
-	    
-		stomach.put(Color.GREEN, nbBamboos) ;
+	@Given("^I have eaten (\\d+) green bamboo chunks$")
+	public void i_have_eaten_n_green_bamboo_chunks(Integer number) {
+
+		stomach.put(Color.GREEN, number) ;
 	}
 
-	@When("^I check if i have validated the objective$")
-	public void i_check_if_i_have_validated_the_objective() {
-		
+	@Given("^I have eaten (\\d+) yellow bamboo chunks$")
+	public void i_have_eaten_n_yellow_bamboo_chunks(Integer number) {
+
+		stomach.put(Color.YELLOW, number) ;
+	}
+
+	@Given("^I have eaten (\\d+) pink bamboo chunks$")
+	public void i_have_eaten_n_pink_bamboo_chunks(Integer number) {
+
+		stomach.put(Color.PINK, number) ;
+	}
+
+	@When("^I check if the \"([^\"]*)\" is completed$")
+	public void i_check_if_the_objective_in_question_is_completed(String objectiveName) {
+
+		pandaObjective = new PandaObjective(Enum.valueOf(PandaObjective.Motif.class, objectiveName));
+
 		when(mockPlayer.getStomach()).thenReturn(stomach);
 		pandaObjective.checkIfCompleted(mockPlayer);
-		
 	}
 
-	@Then("^I should be answered yes$")
-	public void i_should_be_answered_yes() {
-	  
-		assertTrue(pandaObjective.isCompleted());
+	@Then("^the response should be \"([^\"]*)\"$")
+	public void the_response_should_be(String response) {
+
+		System.out.println(response);
 	}
-	
+
 }
