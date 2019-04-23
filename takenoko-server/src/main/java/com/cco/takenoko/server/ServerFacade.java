73b7fb7a5e0e0cb4ff218bcda884d59947c0a3bf
mariliercyril <cyril.marilier@etu.unice.fr>
2019-04-23 09:26:57 +0100

    [#19] - Step 3 (#22): Improve the development of the Java code for JSON

--- takenoko-server/src/main/java/com/cco/takenoko/server/ServerFacade.java
+++ takenoko-server/src/main/java/com/cco/takenoko/server/ServerFacade.java
@@ -1,34 +1,38 @@
 package com.cco.takenoko.server;
 
 import java.awt.Point;
+
 import java.io.IOException;
 
 import com.cco.takenoko.Takenoko;
 
 import com.cco.takenoko.game.Game;
 
+import com.cco.takenoko.game.tiles.Color;
 import com.cco.takenoko.game.tiles.Tile;
 
 import com.cco.takenoko.player.BamBot;
 import com.cco.takenoko.player.Player;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class ServerFacade {
 
-	Game game;
+	private Game game;
 
-	Tile tile;
+	private Tile tile;
 
-	BamBot bamBot;
+	private BamBot bamBot;
 
-	Point point;
+	private Point point;
 
-	ObjectMapper om = new ObjectMapper();
-	ObjectWriter ow = om.writerWithDefaultPrettyPrinter();
+	private ObjectMapper om = new ObjectMapper();
+	private ObjectWriter ow = om.writerWithDefaultPrettyPrinter();
 
 	public ServerFacade() {
 
@@ -43,11 +47,14 @@ public class ServerFacade {
 		}
 	}
 
-	/*			Tile t = this.chooseTile(game);
-                Point choice = this.whereToPutDownTile(game, t);
-                t.setPosition(choice);
-                game.putDownTile(t); */
-
+	////////////////////////////
+	// PUT_DOWN_TILE services //
+	////////////////////////////
+	//-> Tile t = this.chooseTile(game);
+	//-> Point choice = this.whereToPutDownTile(game, t);
+	//-> t.setPosition(choice);
+	//-> game.putDownTile(t);
+	// TODO: Javadoc of this service as a method.
 	public String chooseTile() {
 
 		String tileJson = "";
@@ -57,19 +64,22 @@ public class ServerFacade {
 			tile = new Tile();
 		}
 
+		TileJSON tileJSON = new TileJSON(tile);
+
 		try {
-			tileJson = ow.writeValueAsString(tile);	
-		} catch (JsonProcessingException jpe) {
-			jpe.printStackTrace();
+			tileJson = ow.writeValueAsString(tileJSON);	
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
 
 		return tileJson;
 	}
 
+	// TODO: Javadoc of this service as a method.
 	public String whereToPutDownTile(String tileJson) {
 
 		String pointJson = "";
-		
+
 		if (bamBot != null) {
 			try {
 				Tile t = om.readValue(tileJson, Tile.class);
@@ -83,6 +93,7 @@ public class ServerFacade {
 		return pointJson;
 	}
 
+	// TODO: Javadoc of this service as a method.
 	public boolean putDownTile(String tileJson, String pointJson) {
 
 		try {
@@ -90,12 +101,87 @@ public class ServerFacade {
 			Point p = om.readValue(pointJson, Point.class);
 
 			t.setPosition(p);
-	        game.putDownTile(t);
+			game.putDownTile(t);
 
-	        return true;
+			return true;
 		} catch (IOException ioe) {
 			return false;
 		}
+	}
+
+	/**
+	 * This class allows to get a JSON of a soft version of a Tile object.
+	 * 
+	 * @author cmarilier
+	 */
+	@JsonPropertyOrder({ "position", "color", "irrigable", "irrigated" })
+	private class TileJSON {
+
+		private Point position;
+
+		private Color color;
+
+		private boolean irrigable;
+		private boolean irrigated;
+
+		private TileJSON(Tile tile) {
+
+			this.setPosition(tile);
+
+			this.setColor(tile);
+
+			this.isIrrigable(tile);
+			this.isIrrigated(tile);
+		}
+
+		@JsonSetter("position")
+		private void setPosition(Tile tile) {
+
+			position = tile.getPosition();
+	    }
+
+		@JsonGetter("position")
+		private Point getPosition() {
+
+			return position;
+	    }
+
+		@JsonSetter("color")
+		private void setColor(Tile tile) {
+
+			color = tile.getColor();
+	    }
+
+		@JsonGetter("color")
+		private Color getColor() {
+
+			return color;
+	    }
+
+		@JsonSetter("irrigable")
+		private void isIrrigable(Tile tile) {
+
+			irrigable = tile.isIrrigable();
+	    }
+
+		@JsonGetter("irrigable")
+		private boolean isIrrigable() {
+
+			return irrigable;
+	    }
+
+		@JsonSetter("irrigated")
+		private void isIrrigated(Tile tile) {
+
+			irrigated = tile.isIrrigated();
+	    }
+
+		@JsonGetter("irrigated")
+		private boolean isIrrigated() {
+
+			return irrigated;
+	    }
+
 	}
 
 }
