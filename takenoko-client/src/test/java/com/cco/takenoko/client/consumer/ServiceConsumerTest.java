eeb34b33290892a7348d8adc79838257daefbc56
mariliercyril <cyril.marilier@etu.unice.fr>
2019-05-19 19:08:24 +0100

    [#28] Fix a bug

--- takenoko-client/src/test/java/com/cco/takenoko/client/consumer/ServiceConsumerTest.java
+++ takenoko-client/src/test/java/com/cco/takenoko/client/consumer/ServiceConsumerTest.java
@@ -14,4 +14,4 @@ import cucumber.api.junit.Cucumber;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", plugin = "pretty")
-public class SubscriptionServiceConsumerTest {}
+public class ServiceConsumerTest {}
